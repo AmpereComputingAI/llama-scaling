15a283f01c9876e157038e5d7f94c55ae7051932
Iyappan Subramanian <isubramanian@amperecomputing.com>
2024-09-03 21:44:00 +0000

    Added script

---
+++ stop-container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#for i in {0..8}; do
+#    echo "Welcome, iteration $i!"
+#    docker container stop llama-aio-$i
+#done
+
+docker container stop llama-aio-0 llama-aio-1 llama-aio-2 llama-aio-3 llama-aio-4 llama-aio-5 llama-aio-6 llama-aio-7
